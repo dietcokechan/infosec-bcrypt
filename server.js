ba6e524ddb553c85344cd3bb6704912165dd6f1f
dietcokechan <dietcokechan@gmail.com>
2023-09-15 20:48:38 +0300

    sync

--- server.js
+++ server.js
@@ -15,16 +15,17 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 	console.log(hash);
 	//$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
 	bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-	/*res == true or false*/
+		/*res == true or false*/
 		console.log(res);
 	});
 });
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
